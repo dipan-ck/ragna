f52258a906d408a9b37705398e21322577146fbb
Dipan Chakraborty <dipan2003.dev@gmail.com>
2025-08-05 05:15:02 +0530

    fixed

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express, { Application } from 'express';
-import connectMongoDB from './config/connectMongoDB';
+import connectMongoDB from './config/connectMongoDB.js';
 import authRoutes from './routes/authRoutes';
 import fileRoutes from "./routes/fileRoutes";
 import feedbackRoutes from "./routes/feedbackRoutes"
